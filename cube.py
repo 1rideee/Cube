c9fddd7617918cdad124d607e258817142f01d00
1rideee <einri-o@online.no>
2023-03-03 15:55:41 +0100

    add functions to Cube

--- cube.py
+++ cube.py
@@ -5,6 +5,26 @@ import numpy as np
 class RubiksCube:
     def __init__(self, render) :
         self.render = render
-        self.vertexes = np.array([(0,0,0,1),(0,1,0,1), (1) ])
-    
-        self.faces = np.array()
+        self.vertexes = np.array([(0,0,0,1),(0,1,0,0),(0,0,1,0), (0,0,0,1)])
+        # self.faces = np.array()
+
+    def R(self):
+        pass
+    def L(self):
+        pass
+    def B(self):
+        pass
+    def U(self):
+        pass
+    def D(self):
+        pass 
+    def R_prime(self):
+        pass
+    def L_prime(self):
+        pass
+    def B_prime(self):
+        pass
+    def U_prime(self):
+        pass
+    def D_prime(self):
+        pass 
